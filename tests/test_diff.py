fd9500fc8f50cd44b9b868fec04300f864cddf91
Pavel Ilchenko <arisesinmight@gmail.com>
2023-11-05 16:51:46 +0300

    check badges status

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,15 +1,9 @@
 from gendiff.packages.gendiff import generate_diff
 import json
+from tests.fixtures.result_json import RESULT
 
 
 def test_gendiff():
     file1 = json.load(open('files/file1.json'))
     file2 = json.load(open('files/file2.json'))
-    assert generate_diff(file1, file2) == '''{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}'''
+    assert generate_diff(file1, file2) == RESULT
