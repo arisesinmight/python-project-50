6a67a73a42d4dd7f056da45ddc727f1cc2a42010
Pavel Ilchenko <arisesinmight@gmail.com>
2024-09-02 17:05:37 +0300

    done plain, need to fix tests

--- gendiff/packages/formatters/plain.py
+++ gendiff/packages/formatters/plain.py
@@ -31,9 +31,11 @@ def set_string(path, changes, value=0, old_value=0, new_value=0):
 def draw_changes(diff_dict, path=[]):
     stringed_diff = ""
     keys = sorted(diff_dict.keys())
+
     for key in keys:
         path.append(key)
         stringed_path = f"'{'.'.join(path)}'"
+
         if diff_dict[key]["type"] == "changed":
             stringed_diff += set_string(
                 stringed_path,
@@ -41,15 +43,18 @@ def draw_changes(diff_dict, path=[]):
                 old_value=diff_dict[key]["old_value"],
                 new_value=diff_dict[key]["new_value"]
             )
+            path.remove(key)
+
         elif diff_dict[key]["type"] != "nested":
             stringed_diff += set_string(
                 stringed_path,
                 diff_dict[key]["type"],
                 value=diff_dict[key]["value"],
             )
-        elif diff_dict[key]["type"] == "nested":
-            stringed_diff += draw_changes(diff_dict[key]["value"], path=[key])
+            path.remove(key)
 
-        path.pop()
+        elif diff_dict[key]["type"] == "nested":
+            stringed_diff += draw_changes(diff_dict[key]["value"], path=path)
+            path.remove(key)
 
     return stringed_diff
