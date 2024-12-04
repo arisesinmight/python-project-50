be5e983ed4e12f67104b2187ef8e10169b2babaf
Pavel Ilchenko <arisesinmight@gmail.com>
2024-12-04 12:09:12 +0300

    test fixtures from "py" to "txt"

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,68 +1,67 @@
 from gendiff.packages.generate_diff import generate_diff
-from tests.fixtures.result_plain import plain_nested_result
-from tests.fixtures.result_stylish import stylish_plain_result, stylish_nested_result
-from tests.fixtures.result_json import json_nested_result
-from tests.fixtures.hex_result_plain import hex_plain_result
-from tests.fixtures.hex_result_stylish import hex_stylish_result
 import pytest
 
+def read(file_path):
+    with open(file_path, 'r') as f:
+        result = f.read()
+    return result
 
 @pytest.mark.parametrize(
     "input_file1, input_file2, expected_result, formatter",
     [
         ('tests/fixtures/files/file1.json',
          'tests/fixtures/files/file2.json',
-         stylish_plain_result,
+         read('tests/fixtures/result_stylish_plain.txt'),
          'stylish'),
         ('tests/fixtures/files/file1.yaml',
          'tests/fixtures/files/file2.yaml',
-         stylish_plain_result,
+         read('tests/fixtures/result_stylish_plain.txt'),
          'stylish'),
 
         ('tests/fixtures/files/nested_file1.json',
          'tests/fixtures/files/nested_file2.json',
-         stylish_nested_result,
+         read('tests/fixtures/result_stylish_nested.txt'),
          'stylish'),
         ('tests/fixtures/files/hex_file1.json',
          'tests/fixtures/files/hex_file2.json',
-         hex_stylish_result,
+         read('tests/fixtures/hex_result_stylish.txt'),
          'stylish'),
 
         ('tests/fixtures/files/nested_file1.yaml',
          'tests/fixtures/files/nested_file2.yaml',
-         stylish_nested_result,
+         read('tests/fixtures/result_stylish_nested.txt'),
          'stylish'),
         ('tests/fixtures/files/hex_file1.yml',
          'tests/fixtures/files/hex_file2.yml',
-         hex_stylish_result,
+         read('tests/fixtures/hex_result_stylish.txt'),
          'stylish'),
 
         ('tests/fixtures/files/nested_file1.json',
          'tests/fixtures/files/nested_file2.json',
-         plain_nested_result,
+         read('tests/fixtures/result_plain.txt'),
          'plain'),
         ('tests/fixtures/files/hex_file1.json',
          'tests/fixtures/files/hex_file2.json',
-         hex_plain_result,
+         read('tests/fixtures/hex_result_plain.txt'),
          'plain'),
 
         ('tests/fixtures/files/nested_file1.yaml',
          'tests/fixtures/files/nested_file2.yaml',
-         plain_nested_result,
+         read('tests/fixtures/result_plain.txt'),
          'plain'),
         ('tests/fixtures/files/hex_file1.yml',
          'tests/fixtures/files/hex_file2.yml',
-         hex_plain_result,
+         read('tests/fixtures/hex_result_plain.txt'),
          'plain'),
 
 
         ('tests/fixtures/files/nested_file1.json',
          'tests/fixtures/files/nested_file2.json',
-         json_nested_result,
+         read('tests/fixtures/result_json.txt'),
          'json'),
         ('tests/fixtures/files/nested_file1.yaml',
          'tests/fixtures/files/nested_file2.yaml',
-         json_nested_result,
+         read('tests/fixtures/result_json.txt'),
          'json')
 
     ]
